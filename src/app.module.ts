c1fd3ea7171dabe09ecc7a78629854a743c5fdcf
silica <zergskybmw@gmail.com>
2025-03-15 14:45:02 +0900

    [FEAT] product init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ProductModule } from './product/product.module';
+import Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
